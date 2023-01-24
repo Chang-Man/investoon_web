46e3c5f247d54eb24df69056f03e412410578a3e
Chang-Man <changagim@snu.ac.kr>
2023-01-24 17:15:23 +0900

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, createTheme, ThemeProvider } from "@mui/material";
+import ReactPageScroller from "react-page-scroller";
+import { useState, useEffect } from "react";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Nanum Gothic",
+  },
+});
 
 function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
+  const [currentPage, setCurrentPage] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ height: "calc(var(--vh, 1vh) * 100)" }}>
+        <ReactPageScroller
+          animationTimer={700}
+          pageOnChange={(num) => {
+            setCurrentPage(num);
+          }}
+          customPageNumber={currentPage}
+        ></ReactPageScroller>
+      </Box>
+    </ThemeProvider>
   );
 }
 
